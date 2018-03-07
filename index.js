d8e76ed743264a496ffcd86d841d3403dc6f3707
Julian Grinblat <julian@dotcore.co.il>
2018-03-07 21:50:36 +0900

    Generate sourcemaps

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-module.exports = (main, packages) => ({
+const MagicString = require('magic-string');
+
+module.exports = (main, packages, { sourceMap = true } = {}) => ({
   name: 'polyfill',
-  transform: (source, id) =>
-    packages
-      .filter(() => id.match(main))
-      .map(pkg => `require("${pkg}");`)
-      .join('\n').concat(source)
+  transform: (source, id) => {
+    if (!id.match(main)) return null;
+
+    const magicString = new MagicString(source);
+    magicString.prepend(
+      packages
+        .map(pkg => `require("${pkg}");`)
+        .join('\n') + '\n\n'
+    )
+
+    return {
+      code: magicString.toString(),
+      map: sourceMap ? magicString.generateMap() : null,
+    }
+  }
 })
